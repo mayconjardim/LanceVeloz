b6db580082646af752594db0bbbab54cb5b1839f
Maycon Jardim <mayconpjardim@gmail.com>
2024-02-02 21:21:22 -0300

    Configurando Context no Program e Db no appsettings

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 app.UseAuthorization();
