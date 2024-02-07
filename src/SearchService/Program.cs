b06af09e5b3cba834e5ac8cdf4f4ea2b79f9a36a
Maycon Jardim <mayconpjardim@gmail.com>
2024-02-06 21:24:15 -0300

    Instalando e configurando RabbitMQ com MassTransit

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,7 @@
 using System.Net;
-using Polly;
 using Polly.Extensions.Http;
+using Polly;
+using MassTransit;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -11,6 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
