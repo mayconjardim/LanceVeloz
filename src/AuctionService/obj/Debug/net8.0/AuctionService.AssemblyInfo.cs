c8a09fa57e8ff44c6af1aea70decb13d547d2e8b
Maycon Jardim <mayconpjardim@gmail.com>
2024-02-07 21:21:51 -0300

    Criando contratos

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5da707d2cc14bf15946570df57b8c48beb1cbc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b06af09e5b3cba834e5ac8cdf4f4ea2b79f9a36a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
